5bd7e1b353260f14ff8575a59a8664e36283613f
Shaqur Adair <shaqnificent@gmail.com>
2024-05-07 21:02:25 -0500

    initiated Trainee page

--- src/pages/User/Manager/Trainees/index.jsx
+++ src/pages/User/Manager/Trainees/index.jsx
@@ -1,10 +1,48 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import elipsis from '../../../../assets/trainee/elipsis.svg'
 import './trainees.scss'
 function Trainees() {
   return (
     <div className='traineesContainer'>
-       <h1>Trainees</h1>
+      <div className='tableContainer'>
+        <div
+            style={
+                {
+                    display: "grid",
+                    color: "black",
+                    fontSize: "12px",
+                    gridTemplateColumns: "25% 25% 25% 25%",
+                    padding: "10px 5%",
+                    fontSize: "14px", 
+                    fontWeight: "bold"
+                }
+            }
+        >
+            <span>Name</span>
+            <span>Role</span>
+            <span>Course</span>
+            <span>Status</span>
+        </div>  
+        <div
+            style={
+                {
+                    display: "grid",
+                    color: "black",
+                    fontSize: "12px",
+                    gridTemplateColumns: "25% 25% 25% 25%",
+                    padding: "10px 5%",
+                    fontSize: "12px", 
+                }
+            }
+        >
+            <span style={{ color: "#002868"}}>Gary Black</span>
+            <span>Developer</span>
+            <span>Web1080</span>
+            <span>Pending <span><img src={elipsis} alt="" /></span></span>
+        </div>  
+      </div>
+      
     </div>
   )
 }
